722c21a31290bb225cfe9b62adf1de054c26cc60
Safarqwerty <safar.alam79@gmail.com>
2024-02-16 13:39:43 +0800

    update homepage menampilkan berita dengan template daisyui

--- resources/js/Pages/Homepage.jsx
+++ resources/js/Pages/Homepage.jsx
@@ -1,10 +1,21 @@
-import { Link, Head } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
+import Navbar from '@/Components/Navbar';
+import NewsLists from '@/Components/Homepage/NewsLists';
+import Paginator from '@/Components/Homepage/Paginator';
 
 export default function Homepage(params) {
     // console.log('data = ', params.news)
     return (
-        <div className='flex flex-wrap justify-center p-4 bg-neutral-800'>
-            {params.news ? (
+        <div className='min-h-screen bg-neutral-800'>
+            <Head title={params.title} />
+            <Navbar />
+            <div className='flex justify-center lg:flex-row lg:flex-wrap flex-col lg:items-stretch gap-4 p-4'>
+                <NewsLists news={params.news.data} />
+            </div>
+            <div className='flex justify-center items-center'>
+                <Paginator meta={params.news.meta} />
+            </div>
+            {/* {params.news ? (
                 params.news.map((data, i) => (
                     <div key={i} className="bg-white bg-opacity-10 p-6 rounded-md shadow-md m-4 max-w-sm">
                         <h2 className="text-xl font-bold mb-2">{data.title}</h2>
@@ -15,7 +26,7 @@ export default function Homepage(params) {
                 ))
             ) : (
                 <p className="text-gray-300">Data Kosong</p>
-            )}
+            )} */}
         </div>
     )
 }
