724300e2ba560d1deadc7fef9de123657a6e50b1
Safarqwerty <safar.alam79@gmail.com>
2024-02-16 13:38:14 +0800

    memindahkan fake ke newsfactory

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -6,7 +6,7 @@ use App\Models\News;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
+
 
 class NewsSeeder extends Seeder
 {
@@ -15,15 +15,6 @@ class NewsSeeder extends Seeder
      */
     public function run(): void
     {
-        $faker = Faker::create();
-
-        for($i = 0; $i < 20; $i++) {
-            News::create([
-                'title' => $faker->title(),
-                'description' => $faker->paragraph(2, true),
-                'category' => $faker->sentence(),
-                'author' => $faker->email(),
-            ]);
-        }
+        News::factory()->count(50)->create();
     }
 }
