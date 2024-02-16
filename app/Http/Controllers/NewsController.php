f6f31d87cba7336ca89c0cfcb42a0939c385a82a
Safarqwerty <safar.alam79@gmail.com>
2024-02-16 13:41:54 +0800

    update paginasi

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\NewsCollection;
 use App\Models\News;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -13,8 +14,8 @@ class NewsController extends Controller
      */
     public function index()
     {
-        $news = News::all();
-
+        $news = new NewsCollection(News::paginate(9));
+        // dd($news);
         return Inertia::render('Homepage', [
             'title' => "Assalamu'alaikum",
             'desc' => "Selamat datang",
